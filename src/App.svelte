b2563b907ad3bff0354aea81fb1bc5040091a926
Sung-jun Um <zerodice0@gmail.com>
2021-02-14 16:52:11 +0900

    add: install svelte-routing library. add: about compoenent added. fix: extract Login from to Login.svelte. fix: move Button/Input/Anchor css to global.css. add: transition-duration property is added to Button/Input/Anchor. mod: remove outline from input/button when it is focused.

--- src/App.svelte
+++ src/App.svelte
@@ -1,85 +1,41 @@
 <script>
-	
+	import {Router, Link, Route} from 'svelte-routing'
+	import Login from './routes/Login.svelte'
+	import About from './routes/About.svelte'
+
+	export let url = "";
 </script>
 
-<main>
-	<div class="simple-modal">
-		<div class="mock-bar">
-	    
-		</div>
-
-		<div class="header">
-			<span class="title">HAT</span>
-			<span class='subtitle'>How about this?</span>
-		</div>
-		<div class="body">
-			<input type="text"
-				placeholder="login id"/>
-			<input type="text"
-				placeholder="password"/>
-			<button>
-				Login
-			</button>
-		</div>
+<Router url={url}>
+	<nav class="navigation">
+		<Link class="link" to="/">Home</Link>
+		<Link class="link" to="/about">About</Link>
+	</nav>
+	<div class="router-content">
+		<Route path="/" component="{Login}"/>
+		<Route path="/about" component="{About}"/>
 	</div>
-</main>
+</Router>
 
 <style lang="scss">
-	main {
-		width: 100%;
-		height: 100%;
-		display:flex;
-		justify-content: center;
+	.navigation {
+		display: flex;
 		flex-direction: row;
-		align-content: center;
+		justify-content: start;
 		align-items: center;
-		
-		.simple-modal {
-			display: flexbox;
-			width: 20rem;
-			/* border: 3px solid black; */
-			border-radius: 10px;
-			padding: 10px;
-			background: #ededed;
-			box-shadow: inset 14px 14px 28px #afafaf,
-            inset -14px -14px 28px #ffffff;
-			display: flexbox;
-			flex-direction: column;
+		height: 3rem;
+		border-radius: 7px;
+		padding: 5px;
+		background: #ededed;
+		box-shadow: inset 14px 14px 28px #afafaf,
+					inset -14px -14px 28px #ffffff;
+	
 
-			.header {
-				display:flex;
-				justify-content: center;
-				flex-direction: column;
-				width: 100%;
-				text-align: center;
-				padding-top: 1rem;
-				padding-bottom: 1rem;
-				text-shadow:  4px 4px 6px #afafaf,
-										-4px -4px 6px #ffffff;
-
-				.title {
-					font-size: 1.5rem;
-				}
-			}
-
-			.body {
-				display: flex;
-				flex-direction: column;
-				justify-content: center;
-
-				input, button {
-					margin-left: 1rem;
-					margin-right: 1rem;
-				}
-			}
-
-			button, input {
-				border-radius: 5px;
-				background: #ffffff;
-				box-shadow:  3px 3px 20px #afafaf,
-										-3px -3px 20px #ffffff;
-			}
-		}
 	}
 
+
+	.router-content {
+		width: 100%;
+		height: 100%;
+	}
 </style>
---
+++ src/routes/Login.svelte
@@ -0,0 +1,80 @@
+<script>
+	const login = () => {
+		alert("hello");
+	}
+</script>
+
+<main>
+	<div class="simple-modal">
+		<div class="mock-bar">
+	    
+		</div>
+
+		<div class="header">
+			<span class="title">HAT</span>
+			<span class='subtitle'>How about this?</span>
+		</div>
+		<div class="body">
+			<input type="text"
+				placeholder="login id"/>
+			<input type="text"
+				placeholder="password"/>
+			<button on:click="{login}">
+				Login
+			</button>
+		</div>
+	</div>
+</main>
+
+<style lang="scss">
+	main {
+		width: 100%;
+		height: 100%;
+		display:flex;
+		justify-content: center;
+		flex-direction: row;
+		align-content: center;
+		align-items: center;
+		
+		.simple-modal {
+			display: flexbox;
+			width: 20rem;
+			/* border: 3px solid black; */
+			border-radius: 7px;
+			padding: 7px;
+			background: #ededed;
+			box-shadow: inset 14px 14px 28px #afafaf,
+            inset -14px -14px 28px #ffffff;
+			display: flexbox;
+			flex-direction: column;
+
+			.header {
+				display:flex;
+				justify-content: center;
+				flex-direction: column;
+				width: 100%;
+				text-align: center;
+				padding-top: 1rem;
+				padding-bottom: 1rem;
+				text-shadow:  4px 4px 6px #afafaf,
+										-4px -4px 6px #ffffff;
+
+				.title {
+					font-size: 1.5rem;
+				}
+			}
+
+			.body {
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+
+				input, button {
+					margin-left: 1rem;
+					margin-right: 1rem;
+				}
+			}
+		}
+	}
+
+</style>
